d4e690d101fb3e7dd904987b09a63279ac180677
Muhammad Kamran <kamrankhanmashwani@gmail.com>
2024-07-18 11:17:55 +0500

    new changes

--- app/page.js
+++ app/page.js
@@ -5,6 +5,10 @@ export default function page() {
     <section className='py-5'>
       <div className='container'>
         <h1>Signature app</h1>
+        <p>
+          This app provides the ability to sign requests and responses against
+          different domains
+        </p>
       </div>
     </section>
   )
